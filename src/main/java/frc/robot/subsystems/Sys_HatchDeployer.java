1324bd9178f728f7635290ae0cd6e11b469b336b
dwilson3859 <32660719+dwilson3859@users.noreply.github.com>
2019-02-15 20:17:44 -0800

    Update Sys_HatchDeployer.java

--- src/main/java/frc/robot/subsystems/Sys_HatchDeployer.java
+++ src/main/java/frc/robot/subsystems/Sys_HatchDeployer.java
@@ -1,24 +1,65 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
 
 /**
  * Add your docs here.
  */
 public class Sys_HatchDeployer extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+
+  VictorSPX mHatchMotor;
+  DoubleSolenoid HatchPnuematic;
+  DigitalOutput frontLimitSwitch, backLimitSwitch;
+
+public void configureHatch(){
+      
+  frontLimitSwitch = new DigitalOutput(RobotMap.frontHatchLS);
+  backLimitSwitch = new DigitalOutput(RobotMap.backHatchLS);
+  
+  mHatchMotor = new VictorSPX(RobotMap.shooterWheelsBotVictor);
+  mHatchMotor.configFactoryDefault();
+  mHatchMotor.set(ControlMode.PercentOutput, 0);
+  mHatchMotor.setInverted(false); //set this later
+  mHatchMotor.configVoltageCompSaturation(12.0, Constants.kTimeoutMs);
+  mHatchMotor.enableVoltageCompensation(true); 
+}
+
+public void hatchDeploy(String whereDaHeck){
+  if(whereDaHeck == "forward"){
+    if(frontLimitSwitch.get() == false){
+      mHatchMotor.set(ControlMode.PercentOutput, .2);
+    }
+    if(frontLimitSwitch.get() == true){
+      mHatchMotor.set(ControlMode.PercentOutput, 0);
+    }
+  }
+  if(whereDaHeck == "back"){
+    if(backLimitSwitch.get() == false){
+      mHatchMotor.set(ControlMode.PercentOutput, -.2);
+    }
+    if(backLimitSwitch.get() == true){
+      mHatchMotor.set(ControlMode.PercentOutput, 0);
+    }
+  }
+  if(whereDaHeck == "disable"){
+    mHatchMotor.set(ControlMode.PercentOutput, 0);
+  }
+}
 
   @Override
   public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
+
   }
 }
